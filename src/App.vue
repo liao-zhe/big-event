c4ff4d02ab50acbf6190708a9c39453ffa88990c
lz <1538153125@qq.com>
2023-11-09 10:55:45 +0800

    登录注册表单校验完成

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,7 @@
-<script setup>
-import { useRouter, useRoute } from 'vue-router'
-import { useUserStore, useCountStore } from '@/stores/index'
-import { storeToRefs } from 'pinia'
-import { onMounted } from 'vue'
-
-// 获取路由对象 router useRouter
-// 获取路由参数 route useRoute
-const router = useRouter()
-const route = useRoute()
-console.log(router, route)
-const goList = () => {
-  router.push('/list')
-}
-const userStore = useUserStore()
-const { token } = storeToRefs(userStore)
-const { setToken, removeToken } = userStore
-
-const useCount = useCountStore()
-const { count } = storeToRefs(useCount)
-const { increment } = useCount
-
-onMounted(() => {})
-</script>
+<script setup></script>
 
 <template>
   <router-view></router-view>
-
-  <hr />
-  <div>我是APP组件</div>
-  <!-- 因为使用了element plus 所以components下的组件会自动注册-->
-  <test-com></test-com>
-  <el-button type="primary" @click="$router.push('/home')">跳转首页</el-button>
-  <el-button type="primary" @click="goList()">跳转至列表</el-button>
-
-  <p>{{ token }}</p>
-  <button @click="setToken('tokenkey')">登录</button
-  ><button @click="removeToken()">退出</button>
-
-  <hr />
-  <p>{{ count }}</p>
-  <el-button type="primary" @click="increment()">+</el-button>
 </template>
 
 <style></style>
--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,1 +1,178 @@
-<template>登录注册页面</template>
+<script setup>
+import { ref } from 'vue'
+import { User, Lock } from '@element-plus/icons-vue'
+const isRegister = ref(true)
+const formModel = ref({
+  username: '',
+  password: '',
+  repassword: ''
+})
+//整个表单的校验规则
+//1.非空校验required:true message消息提示，trigger触发校验的时机blur change
+//2.长度校验min:xx,max:xx
+//3.正则校验pattern:正则规则
+const rules = {
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { min: 3, max: 5, message: '长度在3-5个字符', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    {
+      // 最少6位，包括至少1个大写字母，1个小写字母，1个数字，1个特殊字符
+      pattern:
+        /^\S*(?=\S{6,})(?=\S*\d)(?=\S*[A-Z])(?=\S*[a-z])(?=\S*[!@#$%^&*? ])\S*$/,
+      message: '包括至少1个大写字母，1个小写字母，1个数字，1个特殊字符',
+      trigger: 'blur'
+    }
+  ],
+  repassword: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    {
+      // 最少6位，包括至少1个大写字母，1个小写字母，1个数字，1个特殊字符
+      pattern:
+        /^\S*(?=\S{6,})(?=\S*\d)(?=\S*[A-Z])(?=\S*[a-z])(?=\S*[!@#$%^&*? ])\S*$/,
+      message: '包括至少1个大写字母，1个小写字母，1个数字，1个特殊字符',
+      trigger: 'blur'
+    },
+    {
+      validator: (rule, value, callback) => {
+        if (value !== formModel.value.password) {
+          callback(new Error('两次输入密码不一致'))
+        } else {
+          callback()
+        }
+      },
+      trigger: 'blur'
+    }
+  ]
+}
+const form = ref()
+// 注册校验,拿到表单中的validate
+const register = () => {
+  form.value.validate()
+}
+</script>
+
+<template>
+  <el-row class="login-page">
+    <el-col :span="12" class="bg"></el-col>
+    <el-col :span="6" :offset="3" class="form">
+      <!--autocomplete="off" 这将禁用自动完成功能，以确保用户需要手动输入用户名，而不会自动填充以前存储的用户名。 -->
+      <!-- 注册 -->
+      <el-form
+        :rules="rules"
+        :model="formModel"
+        ref="form"
+        size="large"
+        autocomplete="off"
+        v-if="isRegister"
+      >
+        <el-form-item>
+          <h1>注册</h1>
+        </el-form-item>
+        <el-form-item prop="username">
+          <el-input
+            v-model="formModel.username"
+            :prefix-icon="User"
+            placeholder="请输入用户名"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="formModel.password"
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="repassword">
+          <el-input
+            v-model="formModel.repassword"
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请再次输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            class="button"
+            type="primary"
+            @click="register"
+            auto-insert-space
+          >
+            注册
+          </el-button>
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" :underline="false" @click="isRegister = false">
+            ← 返回
+          </el-link>
+        </el-form-item>
+      </el-form>
+      <!-- 登录 -->
+      <el-form ref="form" size="large" autocomplete="off" v-else>
+        <el-form-item>
+          <h1>登录</h1>
+        </el-form-item>
+        <el-form-item>
+          <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            name="password"
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="flex">
+          <div class="flex">
+            <el-checkbox>记住我</el-checkbox>
+            <el-link type="primary" :underline="false">忘记密码？</el-link>
+          </div>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space
+            >登录</el-button
+          >
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" :underline="false" @click="isRegister = true">
+            注册 →
+          </el-link>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+<style lang="scss" scoped>
+.login-page {
+  height: 100vh;
+  background-color: #fff;
+  .bg {
+    background:
+      url('@/assets/logo2.png') no-repeat 60% center / 240px auto,
+      url('@/assets/login_bg.jpg') no-repeat center / cover;
+    border-radius: 0 20px 20px 0;
+  }
+  .form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    user-select: none;
+    .title {
+      margin: 0 auto;
+    }
+    .button {
+      width: 100%;
+    }
+    .flex {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+}
+</style>
